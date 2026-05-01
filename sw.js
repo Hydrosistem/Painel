289013be52f61a54e652aec384575b47815a0278
necroblut <matos.gabriel2025@gmail.com>
2026-05-01 14:34:31 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -45,3 +45,9 @@ self.addEventListener("fetch", (e) => {
     caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
