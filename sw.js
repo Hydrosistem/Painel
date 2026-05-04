65c61e684333007e3780dd00881ffed3ae891e9c
necroblut <matos.gabriel2025@gmail.com>
2026-05-04 09:54:39 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // ═══════════════════════════════════════
 //  HYDROSISTEM SW — AUTO UPDATE
 // ═══════════════════════════════════════
-const CACHE = "hyd-v29";
+const CACHE = "hyd-v30";
 const ASSETS = [
   "./",
   "./index.html",
