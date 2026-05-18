f1a36086b3b433ae625f358e2bdc4d4daad27e1d
Hydrosistem <matos.gabriel2025@gmail.com>
2026-05-18 09:14:17 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // ═══════════════════════════════════════
 //  HYDROSISTEM SW — AUTO UPDATE
 // ═══════════════════════════════════════
-const CACHE = "hyd-v242";
+const CACHE = "hyd-v243";
 const ASSETS = [
   "./",
   "./index.html",
