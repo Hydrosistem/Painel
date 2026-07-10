0e00f7e63b7d72c4d6c9733ccd6a4615c8b1eb47
Hydrosistem <matos.gabriel2025@gmail.com>
2026-07-10 08:58:13 -0300

    Update service worker cache version to v1.8.3

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // ═══════════════════════════════════════
 //  HYDROSISTEM SW — AUTO UPDATE
 // ═══════════════════════════════════════
-const CACHE = "hyd-v1.8.2";
+const CACHE = "hyd-v1.8.3";
 const ASSETS = [
   "./",
   "./index.html",
